1fcb0d11423c2699ae15c98e5efdfe0e5e3f15d1
SnoopCode02 <ayalaalexis982@gmail.com>
2024-05-21 19:43:21 -0300

    actualizacion 21/5/24

--- Algoritmos y  programacion/funciones.py
+++
@@ -1,9 +0,0 @@
-def introducir_entero_valido(mensaje):
-    while True:
-        num = input(mensaje).strip()
-        try:    
-            num = int(num)
-            return int(num)    
-        except ValueError:
-            print(f"El numero: {num} no es un entero, presione enter para continuar.")
-            input()
